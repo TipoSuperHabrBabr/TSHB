2b56e06acb20af74aff7993e4f6b450afd5593b7
Nadezhda <nadvlasova@rambler.ru>
2022-10-09 10:42:29 +0800

    add templates and static

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import views
 from django.contrib import admin
 from django.urls import path
 
+from blogapp.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
+
 ]
