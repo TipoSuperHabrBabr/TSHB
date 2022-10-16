0a324e9a8262501ca70ed93d5a20a1f601408e96
Onlyjoysprod <kujaku@yandex.ru>
2022-10-16 12:04:05 +0300

    fix views/urls/inc_menu

--- backend/blogapp/urls.py
+++ backend/blogapp/urls.py
@@ -4,6 +4,5 @@ from blogapp import views as blogapp
 app_name = 'blogapp'
 
 urlpatterns = [
-    path('', blogapp.index, name='index'),
-    path('category/<int:pk>/', blogapp.blog, name='category'),
+    path('category', blogapp.blog, name='category'),
 ]
--- backend/blogapp/views.py
+++ backend/blogapp/views.py
@@ -8,12 +8,20 @@ def index(request):
                   )
 
 
-def blog(request, pk=None):
-    category = {0: 'Все',
-                1: 'Дизайн',
-                2: 'Веб-разработка',
-                3: 'Мобильная разработка',
-                4: 'Маркетинг'}
+def blog(request):
+    categories = {
+                '0': 'Все категории',
+                '1': 'Дизайн',
+                '2': 'Веб-разработка',
+                '3': 'Мобильная разработка',
+                '4': 'Маркетинг'
+    }
+    category_id = request.GET.get('id')
+    current_category = categories[category_id]
+    context = {
+        'categories': categories,
+        'current_category': current_category,
+        'title': current_category,
+    }
 
-    return render(request, 'blogapp/blog.html',
-                  context={'category': category})
+    return render(request, 'blogapp/blog.html', context)
