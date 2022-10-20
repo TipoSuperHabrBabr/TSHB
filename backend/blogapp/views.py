8a9652a6be2dc76e0eaee2916d83874d77ce57ca
YuryRazhkov <yrozhkov1983@gmail.com>
2022-10-20 07:20:03 +0300

    refactor views (nesting conditions deleted), edit tamplates

--- backend/blogapp/views.py
+++ backend/blogapp/views.py
@@ -1,14 +1,13 @@
 from authapp.models import BlogUser
 from blogapp.forms import PostForm
 from blogapp.models import Post
-from django import http
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect, HttpResponseForbidden, HttpResponse
-from django.shortcuts import render, get_object_or_404, redirect
-from django.urls import reverse, reverse_lazy
+from django.http import HttpResponseRedirect, HttpResponseForbidden
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 from django.utils import timezone
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DeleteView
+from django.views.generic import CreateView
 
 
 # Create your views here.
@@ -20,11 +19,11 @@ def index(request):
 
 def blog(request):
     categories = {
-                '0': 'Все категории',
-                '1': 'Дизайн',
-                '2': 'Веб-разработка',
-                '3': 'Мобильная разработка',
-                '4': 'Маркетинг'
+        '0': 'Все категории',
+        '1': 'Дизайн',
+        '2': 'Веб-разработка',
+        '3': 'Мобильная разработка',
+        '4': 'Маркетинг'
     }
     category_id = request.GET.get('id')
     current_category = categories[category_id]
@@ -58,43 +57,44 @@ class PostCreateView(CreateView):
     def get_success_url(self, **kwargs):
         return reverse('index')
 
+
 @login_required
 def edit_post(request, pk):
     post = Post.objects.get(id=pk)
-
-    if request.user == post.user_id and post.is_active == True: # проверяем соответствие юзера из поста и реквеста
-        if request.method == 'POST':
-
-            form = PostForm(request.POST or None, instance=post)
-            if form.is_valid():
-                form.save()
-                post.edit_date = timezone.now()
-                post.save()
-                return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-        else:
-            form = PostForm(request.POST or None, instance=post)
-
-        content = {
-            'title': 'Редактирование статьи',
-            'form': form,
-            'post': post,
-        }
-        return render(request, 'blogapp/edit_post.html', content)
-
-    else:
+    print(request.user != post.user_id)
+    print(post.is_active == True)
+    if request.user != post.user_id or post.is_active is not True:  # проверяем доступ юзера и статус поста
         return HttpResponseForbidden('Permission Error')
+
+    if request.method == 'POST':
+
+        form = PostForm(request.POST or None, instance=post)
+        if form.is_valid():
+            form.save()
+            post.edit_date = timezone.now()
+            post.save()
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    else:
+        form = PostForm(request.POST or None, instance=post)
+
+    content = {
+        'title': 'Редактирование статьи',
+        'form': form,
+        'post': post,
+    }
+    return render(request, 'blogapp/edit_post.html', content)
+
 
 @login_required
 def delete_post(request, pk):
     post = Post.objects.get(id=pk)
 
-    if request.user == post.user_id and post.is_active == True: # проверяем соответствие юзера из поста и реквеста
-        if request.method == "POST":
-            post.delete()
-            return HttpResponseRedirect(reverse('index'))
-
-        context = {'object': post}
-        return render(request, 'blogapp/post_confirm_delete.html', context)
-
-    else:
+    if request.user != post.user_id or post.is_active is not True:  # проверяем доступ юзера и статус поста
         return HttpResponseForbidden('Permission Error')
+
+    if request.method == "POST":
+        post.delete()
+        return HttpResponseRedirect(reverse('index'))
+
+    context = {'object': post}
+    return render(request, 'blogapp/post_confirm_delete.html', context)
